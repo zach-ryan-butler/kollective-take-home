febc2a009e32d03cab0477d67ffd85d02daf324b
Zachery Butler <zacherybutler@Zachs-MacBook-Pro.local>
2022-04-21 13:23:35 -0600

    add routing between pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from "react";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+
+// components
+import CatchPokemonPage from "./pages/catchPokemonPage/CatchPokemonPage";
+import CaughtPokemonPage from "./pages/caughtPokemonPage/CaughtPokemonPage";
+import PokemonDetailsPage from "./pages/pokemonDetailsPage/PokemonDetailsPage";
+
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <main>
+      <Router>
+        <Routes>
+          <Route path="/" element={<CaughtPokemonPage />} />
+          <Route path="catch" element={<CatchPokemonPage />} />
+          <Route path="details/:id" element={<PokemonDetailsPage />} />
+        </Routes>
+      </Router>
+    </main>
   );
 }
 
